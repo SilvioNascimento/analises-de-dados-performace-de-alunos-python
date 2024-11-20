c15d6c55884eb3e306a348f1c8bcacafd12acc0b
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-20 09:19:13 -0300

    feat: filtrando os dados para serem exibidos através da pontuação de matemática

--- main.py
+++ main.py
@@ -3,5 +3,12 @@ import pandas as pd
 
 st.set_page_config(layout="wide")
 
-students_performance = pd.read_csv("./datasets/Cleaned_Students_Performance.csv")
-students_performance
+df_students_performance = pd.read_csv("./datasets/Cleaned_Students_Performance.csv")
+
+math_score_max = df_students_performance["math_score"].max()
+math_score_min = df_students_performance["math_score"].min()
+max_math_score = st.sidebar.slider("Math Score", math_score_min, math_score_max, math_score_max)
+
+df_scores = df_students_performance[df_students_performance["math_score"] <= max_math_score]
+df_scores
+
