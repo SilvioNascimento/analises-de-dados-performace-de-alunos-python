a76905d92b744a466022bbbd89e11b66b0a2f8a0
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2024-11-20 10:20:59 -0300

    feat: filtrando os dados para serem exibidos através da pontuações de matemática, leitura e esrita

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ reading_score_max = df_students_performance["reading_score"].max()
 reading_score_min = df_students_performance["reading_score"].min()
 max_reading_score = st.sidebar.slider("Reading Score", reading_score_min, reading_score_max, reading_score_max)
 
-df_scores = df_students_performance[(df_students_performance["math_score"] <= max_math_score) & (df_students_performance["reading_score"] <= max_reading_score)]
+writing_score_max = df_students_performance["writing_score"].max()
+writing_score_min = df_students_performance["writing_score"].min()
+max_writing_score = st.sidebar.slider("Writing Score", writing_score_min, writing_score_max, writing_score_max)
+
+df_scores = df_students_performance[(df_students_performance["math_score"] <= max_math_score) & (df_students_performance["reading_score"] <= max_reading_score) & (df_students_performance["writing_score"] <= max_writing_score)]
 df_scores
 
